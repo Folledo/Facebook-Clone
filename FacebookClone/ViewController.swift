d1db5e398322e397a79a5ac19486381fd93fdba0
SwiftKobe <folledoswag@gmail.com>
2018-10-08 21:18:11 -0400

    Initial Commit

---
+++ FacebookClone/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  FacebookClone
+//
+//  Created by Samuel Folledo on 10/8/18.
+//  Copyright © 2018 Samuel Folledo. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
